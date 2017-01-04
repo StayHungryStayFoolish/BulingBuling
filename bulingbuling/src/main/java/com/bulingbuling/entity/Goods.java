368d4b94ffb2eee99a3a908226feb4d77e7f2130
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-05 00:42:52 +0800

    commit

--- bulingbuling/src/main/java/com/bulingbuling/entity/Goods.java
+++ bulingbuling/src/main/java/com/bulingbuling/entity/Goods.java
@@ -7,4 +7,21 @@ package com.bulingbuling.entity;
  * 商品
  */
 public class Goods {
+
+    // 商品 ID
+    private String goodsId;
+
+    // 商品名字
+    private String goodsName;
+
+    // 商品价格
+    private String goodsPrice;
+
+    // 商品图片地址
+    private String goodsImg;
+
+    // 商品库存
+    private String goodsStock;
+
+
 }
