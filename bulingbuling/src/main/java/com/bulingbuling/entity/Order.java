62ea3bdc5e8a91c89f4adb8f1ebf04778ba6d97c
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-07 14:23:14 +0800

    commit

---
+++ bulingbuling/src/main/java/com/bulingbuling/entity/Order.java
@@ -0,0 +1,37 @@
+package com.bulingbuling.entity;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午2:18 on 17/1/7.
+ * <p>
+ * 订单
+ */
+public class Order {
+
+    // 订单
+    private String orderId;
+
+    // 订单人名字
+    private String orderUserName;
+
+    // 订单商品名字
+    private String orderGoodsName;
+
+    // 订单商品图片
+    private String orderGoodsImg;
+
+    // 订单价格
+    private String orderPrice;
+
+    // 订单状态
+    private String orderStatus;
+
+    // 订单时间
+    private String orderTime;
+
+    // 订单收货地址
+    private String orderAddress;
+
+
+
+}
