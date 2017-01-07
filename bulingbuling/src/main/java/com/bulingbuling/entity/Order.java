40fa692c065b1b4959435c726eb3dde29a92864f
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-07 14:23:33 +0800

    commit

--- bulingbuling/src/main/java/com/bulingbuling/entity/Order.java
+++ bulingbuling/src/main/java/com/bulingbuling/entity/Order.java
@@ -32,6 +32,67 @@ public class Order {
     // 订单收货地址
     private String orderAddress;
 
+    public String getOrderId() {
+        return orderId;
+    }
 
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
 
+    public String getOrderUserName() {
+        return orderUserName;
+    }
+
+    public void setOrderUserName(String orderUserName) {
+        this.orderUserName = orderUserName;
+    }
+
+    public String getOrderGoodsName() {
+        return orderGoodsName;
+    }
+
+    public void setOrderGoodsName(String orderGoodsName) {
+        this.orderGoodsName = orderGoodsName;
+    }
+
+    public String getOrderGoodsImg() {
+        return orderGoodsImg;
+    }
+
+    public void setOrderGoodsImg(String orderGoodsImg) {
+        this.orderGoodsImg = orderGoodsImg;
+    }
+
+    public String getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(String orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(String orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public String getOrderAddress() {
+        return orderAddress;
+    }
+
+    public void setOrderAddress(String orderAddress) {
+        this.orderAddress = orderAddress;
+    }
 }
