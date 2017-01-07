cc73ac285ffd8d5f38716304eb5fc3f430dcbd6e
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-07 14:15:48 +0800

    commit

---
+++ bulingbuling/src/main/java/com/bulingbuling/entity/Message.java
@@ -0,0 +1,87 @@
+package com.bulingbuling.entity;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午2:11 on 17/1/7.
+ * <p>
+ * 留言
+ */
+public class Message {
+
+    // 留言 ID
+    private String messgaeId;
+
+    // 留言视频 ID
+    private String messgaeVideoId;
+
+    // 留言用户 ID
+    private String messgaeUserId;
+
+    // 留言用户名字
+    private String messgaeUserName;
+
+    // 留言内容
+    private String messgaeContent;
+
+    // 留言时间
+    private String messgaeTime;
+
+    // 留言用户头像
+    private String messgaeAvatar;
+
+    public String getMessgaeId() {
+        return messgaeId;
+    }
+
+    public void setMessgaeId(String messgaeId) {
+        this.messgaeId = messgaeId;
+    }
+
+    public String getMessgaeVideoId() {
+        return messgaeVideoId;
+    }
+
+    public void setMessgaeVideoId(String messgaeVideoId) {
+        this.messgaeVideoId = messgaeVideoId;
+    }
+
+    public String getMessgaeUserId() {
+        return messgaeUserId;
+    }
+
+    public void setMessgaeUserId(String messgaeUserId) {
+        this.messgaeUserId = messgaeUserId;
+    }
+
+    public String getMessgaeUserName() {
+        return messgaeUserName;
+    }
+
+    public void setMessgaeUserName(String messgaeUserName) {
+        this.messgaeUserName = messgaeUserName;
+    }
+
+    public String getMessgaeContent() {
+        return messgaeContent;
+    }
+
+    public void setMessgaeContent(String messgaeContent) {
+        this.messgaeContent = messgaeContent;
+    }
+
+    public String getMessgaeTime() {
+        return messgaeTime;
+    }
+
+    public void setMessgaeTime(String messgaeTime) {
+        this.messgaeTime = messgaeTime;
+    }
+
+    public String getMessgaeAvatar() {
+        return messgaeAvatar;
+    }
+
+    public void setMessgaeAvatar(String messgaeAvatar) {
+        this.messgaeAvatar = messgaeAvatar;
+    }
+}
