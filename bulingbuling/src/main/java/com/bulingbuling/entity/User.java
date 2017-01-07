ad178834dbecd699cd2cef26cc6713f14982ea99
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-07 14:44:35 +0800

    commit

---
+++ bulingbuling/src/main/java/com/bulingbuling/entity/User.java
@@ -0,0 +1,59 @@
+package com.bulingbuling.entity;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午4:32 on 17/1/7.
+ */
+public class User {
+
+    // 用户 ID
+    private String userId;
+
+    // 用户真实名字
+    private String userTrueName;
+
+    // 用户名
+    private String userName;
+
+    // 密码
+    private String passWord;
+
+    // 银行卡
+    private String bankCard;
+
+    // 性别
+    private String gender;
+
+    // 头像
+    private String avatar;
+
+    // 地址
+    private String address;
+
+    // 手机
+    private String phone;
+
+    // QQ
+    private String qq;
+
+    // Email
+    private String email;
+
+    // 收藏
+    private String mark;
+
+    // 状态
+    private String status;
+
+    // 最后登录时间
+    private String lastLogin;
+
+    // 最后登录 ID
+    private String lastIP;
+
+    // 支付密码
+    private String payPassword;
+
+    // 账户余额
+    private String balance;
+}
