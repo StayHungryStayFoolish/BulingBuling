ef5c9755e4a3120a695d0e63917dbe180078953a
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-07 14:31:39 +0800

    commit

---
+++ bulingbuling/src/main/java/com/bulingbuling/entity/ShoppingCart.java
@@ -0,0 +1,25 @@
+package com.bulingbuling.entity;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午2:26 on 17/1/7.
+ * <p>
+ * 购物车
+ */
+public class ShoppingCart {
+
+    // 购物车 ID
+    private String cartId;
+
+    // 购买用户名字
+    private String userName;
+
+    // 购买商品 ID
+    private String shoppingId;
+
+    // 商品图片地址
+    private String shoppingImg;
+
+    // 商品价格
+    private String shoppingPrice;
+}
