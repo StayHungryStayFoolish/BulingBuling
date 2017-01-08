96665e2eb85ce549216306b69f4aaca7b04ac44f
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-08 22:50:08 +0800

    commit

---
+++ bulingbuling/src/main/java/com/bulingbuling/entity/Video.java
@@ -0,0 +1,40 @@
+package com.bulingbuling.entity;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:45 on 17/1/8.
+ * <p>
+ * 视频
+ */
+public class Video {
+
+    // 视频 ID
+    private String videoId;
+
+    // 视频
+    private String videoName;
+
+    // 视频图片
+    private String videoImg;
+
+    // 视频地址
+    private String videoAddress;
+
+    // 视频时长
+    private String videoLength;
+
+    // 收藏
+    private String videoMark;
+
+    // 留言
+    private String videoMessage;
+
+    // 上传时间
+    private String videoTime;
+
+    // 状态
+    private String videoStatus;
+
+    // 分类
+    private String videoCategory;
+}
