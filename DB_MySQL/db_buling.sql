b90151635b76cee0d8d5754d30771d41e58c8662
StayHungryStayFoolish <bonismo@hotmail.com>
2017-10-16 01:50:28 +0800

    commit

--- DB_MySQL/db_buling.sql
+++ DB_MySQL/db_buling.sql
@@ -2,3 +2,7 @@ DROP DATABASE IF EXISTS db_buling;
 CREATE DATABASE db_buling;
 USE db_buling;
 
+DROP TABLE if EXISTS db_buling.forum;
+CREATE TABLE db_buling.forum(
+  id INT AUTO_INCREMENT PRIMARY KEY COMMENT ''
+);
